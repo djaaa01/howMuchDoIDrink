f4b05bb9e550378b7436041e78c35675c35a2461
Arpi <arpadbacos@gmail.com>
2025-05-18 18:47:22 +0300

    wip

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [],
+  imports: [TranslatePipe],
 })
 export class HomePage {}
